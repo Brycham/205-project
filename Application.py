ed8c04c67c128895e6c712c77aca891c5b78a420
Luis Alva <lalva@csumb.edu>
2018-05-12 16:14:07 -0700

    new code

--- Application.py
+++ Application.py
@@ -1,6 +1,16 @@
+'''
+    
+    Author: Luis Alva
+    
+    Description: This is the application window that will call other files in the same directory and put the images accordingly to where they need to be. 
+    
+    
+    '''
+
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QVBoxLayout, QLabel, QComboBox, QVBoxLayout, QHBoxLayout, QInputDialog, QFileDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QVBoxLayout, QLabel, QComboBox, QVBoxLayout, QHBoxLayout, QInputDialog, QFileDialog, QComboBox, QLineEdit, QHBoxLayout
 from PyQt5.QtCore import pyqtSlot
+import PastMust as reco
 import reco as recognize
 
 from PyQt5.QtGui import QIcon
@@ -14,6 +24,10 @@ app = QApplication(sys.argv)
 temp = ""
 temp1 = ""
 
+temp2 = ""
+
+my_list = ["Face", "Mouth", "Eyes"]
+
 class NewWindow1(QWidget):
     def __init__(self):
         super().__init__()
@@ -24,9 +38,10 @@ class NewWindow1(QWidget):
         qbox = QVBoxLayout()
         
         
+        
         self.label1 = QLabel('Choose your images to Plaster')
-        self.btn = QPushButton("Choose Image", self)
-        self.btn1 = QPushButton("Choose Image", self)
+        self.btn = QPushButton("Choose Face(s)", self)
+        self.btn1 = QPushButton("Choose a Mustache", self)
         self.btn2 = QPushButton("Run", self)
      
         
@@ -44,21 +59,40 @@ class NewWindow1(QWidget):
         self.setLayout(qbox)
     
     
+    def crop(self,sentence):
+        path = ""
+        for letter in sentence:
+            if(letter == "/"):
+                path = ""
+            else:
+                path += letter
+        return path
+    
     def on_click2(self):
-        print(temp)
-        print(temp1)
+        reco.pastFunction(temp, temp1)
+    
     
     
     
     
     def on_click(self):
+       
         global temp
         temp = self.openFileNameDialog()
+        tempi = self.crop(temp)
+        self.btn.setText(tempi)
+     
+     
 
     
     def on_click1(self):
         global temp1
         temp1 = self.openFileNameDialog()
+        tempi = self.crop(temp1)
+        self.btn1.setText(tempi)
+    
+    
+
 
 
 
@@ -77,24 +111,59 @@ class NewWindow(QWidget):
         super().__init__()
         
         
-        self.setWindowTitle('Face Warp')
+
+        self.setWindowTitle('Face Recognition')
         
         qbox = QVBoxLayout()
         
+        global my_list
         
-        self.label1 = QLabel('Choose your images')
-        self.btn = QPushButton("Choose Image", self)
+        self.my_combo_box = QComboBox()
+        
+        self.my_combo_box.addItems(my_list)
+        
+        self.label1 = QLabel('Choose your Face(s)')
+      
+        self.btn = QPushButton("Choose a Face(s)", self)
+        self.btn1 = QPushButton("Run", self)
         
         
         qbox.addWidget(self.label1)
         qbox.addWidget(self.btn)
+        qbox.addWidget(self.my_combo_box)
+        qbox.addWidget(self.btn1)
         
         self.btn.clicked.connect(self.on_click)
+        self.btn1.clicked.connect(self.on_click4)
+        
         self.setLayout(qbox)
     
     
+    
+    def crop(self,sentence):
+        path = ""
+        for letter in sentence:
+            if(letter == "/"):
+                path = ""
+            else:
+                path += letter
+        return path
+    
+    
     def on_click(self):
-        recognize.recognize(self.openFileNameDialog())
+        global temp2
+        temp2 = self.openFileNameDialog()
+        tempi = self.crop(temp2)
+        self.btn.setText(tempi)
+    
+    
+    
+    def on_click4(self):
+        global temp2
+        recognize.recognize(temp2)
+    
+    
+    
     
     
 
@@ -119,15 +188,18 @@ class MainWindow(QWidget):
         
         vbox = QVBoxLayout()
         
-        self.my_combo_box = QComboBox()
+        #self.my_combo_box = QComboBox()
+        
         self.label1 = QLabel('Which would you like to do?')
         
-        self.my_btn = QPushButton("Face Warp", self)
+        self.my_btn = QPushButton("Facial Recognize", self)
         self.my_btn1 = QPushButton("Plaster", self)
         
         vbox.addWidget(self.label1)
+        
         vbox.addWidget(self.my_btn)
         vbox.addWidget(self.my_btn1)
+        #vbox.addWidget(self.my_combo_box)
         
         self.my_btn.clicked.connect(self.on_click)
         self.my_btn1.clicked.connect(self.on_click1)
@@ -143,8 +215,7 @@ class MainWindow(QWidget):
     #@pyqtSlot()
     def on_click(self):
         
-        # i = self.my_combo_box.currentIndex()
-        # if i!=0:
+     
         self.new_win = NewWindow()
         self.new_win.show()
 
--- PastMust.py
+++ PastMust.py
@@ -21,28 +21,31 @@ def pastFilterCV(faces,settings,toPast):    #This function is pasting a resized 
                 faces[x + settings[1] + int(settings[3] * 0.6), y + settings[0] + int(settings[2] * 0.25)] = (toPast[x,y][0], toPast[x,y][1], toPast[x,y][2])
     cv2.imwrite('facesmustached.jpg', faces)
 
-casc_class = 'haarcascade_frontalface_default.xml'
-face_cascade = cv2.CascadeClassifier(casc_class)
-
-if face_cascade.empty():
-    print('# WARNING: Cascade did not load')
-
-img = cv2.imread('Faces.jpg')
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 
-faces = face_cascade.detectMultiScale(gray, 1.1, 9)
+def pastFunction(path1, path2):
+    casc_class = 'haarcascade_frontalface_default.xml'
+    face_cascade = cv2.CascadeClassifier(casc_class)
 
-#for(x,y,h,w) in faces:
-#    cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+    if face_cascade.empty():
+        print('# WARNING: Cascade did not load')
 
-must = cv2.imread('mustache.png', cv2.IMREAD_UNCHANGED)
-for(x,y,w,h) in faces:
-    newMustache = cv2.resize(must, (int(w * 0.5), int(h * 0.1)))
-
-    toPast = newMustache
-    Faces = img
-    pastFilterCV(Faces,(x,y,w,h), toPast)
+    img = cv2.imread(path1)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 
-Image.open('facesmustached.jpg').show()
+    faces = face_cascade.detectMultiScale(gray, 1.1, 9)
+
+
+    must = cv2.imread(path2, cv2.IMREAD_UNCHANGED)
+    for(x,y,w,h) in faces:
+        newMustache = cv2.resize(must, (int(w * 0.5), int(h * 0.1)))
+
+        toPast = newMustache
+        Faces = img
+        pastFilterCV(Faces,(x,y,w,h), toPast)
+
+
+    Image.open('facesmustached.jpg').show()
+
+
